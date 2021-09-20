67d59bc26639c1441ad158dd0e1ea66ede247cf8
simplex3510 <leeyj6457@naver.com>
2021-09-20 20:08:29 +0900

    실습 3일차

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,10 +6,40 @@ public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            // Stop Speed
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.3f, rigid.velocity.y);
+        }
+
+        // Direction Sprite
+        if (Input.GetButton("Horizontal"))
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1 ? true : false;
+        }
+
+        // Animation
+        if (Mathf.Abs(rigid.velocity.x) <= 0.3)
+        {
+            animator.SetBool("isWalk", false);
+        }
+        else
+        {
+            animator.SetBool("isWalk", true);
+        }
+
     }
 
     void FixedUpdate()
@@ -18,7 +48,7 @@ public class PlayerMove : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
-        
+        // Max Speed
         if(rigid.velocity.x > maxSpeed)             // right Max Speed
         {
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);

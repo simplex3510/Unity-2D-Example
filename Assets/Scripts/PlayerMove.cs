cc3c1fa1b0d7a69369b0a626a6c1fb085cde7623
simplex3510 <leeyj6457@naver.com>
2021-09-21 20:52:07 +0900

    4일차 실습

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator animator;
@@ -18,10 +19,17 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
+        // Jump
+        if (Input.GetButtonDown("Jump") && (rigid.velocity.y == 0))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            animator.SetBool("isJump", true);
+        }
+
         if (Input.GetButtonUp("Horizontal"))
         {
             // Stop Speed
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.3f, rigid.velocity.y);
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.1f, rigid.velocity.y);
         }
 
         // Direction Sprite
@@ -49,13 +57,27 @@ public class PlayerMove : MonoBehaviour
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
         // Max Speed
-        if(rigid.velocity.x > maxSpeed)             // right Max Speed
+        if (rigid.velocity.x > maxSpeed)             // right Max Speed
         {
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         }
-        else if (rigid.velocity.x < maxSpeed*(-1))  // Left Max Speed
+        else if (rigid.velocity.x < maxSpeed * (-1))  // Left Max Speed
         {
-            rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+        }
+
+        // Landing Platform
+        if (rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down / 2, new Color(1, 0, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector2.down, 1, LayerMask.GetMask("Platform"));
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance <= 0.5)
+                {
+                    animator.SetBool("isJump", false);
+                }
+            }
         }
     }
 }
